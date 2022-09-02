4d1bb09b4470754703a0c82bb27145914f93c231
featuresnap <pkelly14159@hotmail.com>
2022-09-02 12:21:28 +0000

    keypad sends keypress event to mediator

--- components/calculator-keypad/calculator-keypad.js
+++ components/calculator-keypad/calculator-keypad.js
@@ -3,6 +3,29 @@ export default class CalculatorKeypad extends HTMLElement {
     constructor() {
         super();
     }
+
+    connectedCallback() {
+        this.buttons = {};
+        const root = this;
+        const allClearButton = document.createElement('button');
+        allClearButton.innerText = "AC";
+        this.button.AC = allClearButton;
+        root.appendChild(allClearButton);
+        allClearButton.addEventListener('click', () => {
+            if (this.mediator) {
+                this.mediator.tell({'type': 'keyPress'});
+            }
+        })
+        
+    }
+
+    registerMediator(mediator) {
+        this.mediator = mediator;
+    }
+
+    get button() {
+        return this.buttons;
+    }
 }
 
 if (!customElements.get('calculator-keypad')) {
--- test/__tests__/component/calculator-keypad-test.js
+++ test/__tests__/component/calculator-keypad-test.js
@@ -2,9 +2,34 @@
  * @jest-environment jsdom
  */
  import {CalculatorKeypad} from '../../../components/calculator-keypad/calculator-keypad';
- import '@testing-library/jest-dom/extend-expect';
- 
+ import '@testing-library/jest-dom';
+ import userEvent from '@testing-library/user-event';
+
+ let keypad; 
+
+ beforeAll(() => {
+    keypad = document.body.appendChild(document.createElement('calculator-keypad'));
+ });
+
  test('exists', () => {
     const keypad = document.body.appendChild(document.createElement('calculator-keypad'));
     expect(keypad).toBeVisible();
  });
+
+ test('clicking "All Clear" button publishes all clear', async () => {
+    let captured;
+    let user = userEvent.setup();
+    let mediator = {
+        tell: e => captured = e
+    }
+
+    keypad.registerMediator(mediator);
+
+    const allClear = keypad.button['AC'];
+    await user.click(allClear);
+
+    expect(captured).toEqual({type:'keyPress'});
+
+ })
+
+ afterAll(() => keypad.remove());
