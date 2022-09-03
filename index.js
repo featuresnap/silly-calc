e732778f6bd2ccf68d53be72eeb957f086705113
featuresnap <pkelly14159@hotmail.com>
2022-09-03 11:59:34 +0000

    Test cleanup; add keypad to default index page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import LedDisplay from './components/led-display/led-display.js'
+import CalculatorKeypad from './components/calculator-keypad/calculator-keypad.js'
 
 console.log('hello')
 
--- test/__tests__/component/calculator-keypad-test.js
+++ test/__tests__/component/calculator-keypad-test.js
@@ -1,35 +1,42 @@
 /**
  * @jest-environment jsdom
  */
- import {CalculatorKeypad} from '../../../components/calculator-keypad/calculator-keypad';
- import '@testing-library/jest-dom';
- import userEvent from '@testing-library/user-event';
+import { CalculatorKeypad } from '../../../components/calculator-keypad/calculator-keypad';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 
- let keypad; 
+let keypad;
+let mediator;
+let captured;
 
- beforeAll(() => {
+beforeAll(() => {
     keypad = document.body.appendChild(document.createElement('calculator-keypad'));
- });
 
- test('exists', () => {
-    const keypad = document.body.appendChild(document.createElement('calculator-keypad'));
-    expect(keypad).toBeVisible();
- });
-
- test('clicking "All Clear" button publishes all clear', async () => {
-    let captured;
-    let user = userEvent.setup();
-    let mediator = {
+    mediator = {
         tell: e => captured = e
-    }
+    };
 
     keypad.registerMediator(mediator);
+});
+
+beforeEach(() => {
+    captured = null;
+});
+
+test('exists', () => {
+    const keypad = document.body.appendChild(document.createElement('calculator-keypad'));
+    expect(keypad).toBeVisible();
+});
+
+test('clicking "All Clear" button publishes all clear', async () => {
+
+    let user = userEvent.setup();
 
     const allClear = keypad.button['AC'];
     await user.click(allClear);
 
-    expect(captured).toEqual({type:'keyPress'});
+    expect(captured).toEqual({ type: 'keyPress' });
 
- })
+});
 
- afterAll(() => keypad.remove());
+afterAll(() => keypad.remove());
